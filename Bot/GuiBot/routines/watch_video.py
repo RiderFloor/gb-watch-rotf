b5d8336880b66196167324a370e3780915b61a27
RiderFloor <90529758+RiderFloor@users.noreply.github.com>
2022-11-21 01:45:15 +0700

    Update watch_video.py

--- Bot/GuiBot/routines/watch_video.py
+++ Bot/GuiBot/routines/watch_video.py
@@ -26,7 +26,7 @@ class StreamZZ:
 
                 if self.start_watching():
                     print_log(f'Watch.watch - is watching', level='MESS')
-                    t = 8
+                    t = 10
                     print_log(f'Watch.watch - waiting {t}min: {dt.now()}', level='MESS')
                     wait(t*60)  # video length
                     print_log(f'Watch.watch - finished watching', level='MESS')
